45c9b215c5928d250a958e8af885cfab507ccfda
yi-dai <851758415@qq.com>
2020-08-10 23:22:50 +0800

    edit code to satisfied

--- 3-object/values/index.js
+++ 3-object/values/index.js
@@ -1,10 +1,14 @@
-import { restElement } from '@babel/types';
-
 export default function countTypesNumber(source) {
   // TODO 6: 在这里写实现代码
+  /*
   let result = 0;
   for (const element in source) {
-    result += parseInt(source[element]);
+    result += parseInt(source[element], 10);
   }
+  */
+  let result = 0;
+  Object.values(source).forEach(element => {
+    result += parseInt(element, 10);
+  });
   return result;
 }
